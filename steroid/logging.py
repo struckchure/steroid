e762a10ea7e9afd8d4e5cfdea8a1cc6714ef2a10
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-07-20 15:41:03 +0100

    feat(logging): adds log configurations

---
+++ steroid/logging.py
@@ -0,0 +1,40 @@
+import logging
+from logging.config import dictConfig
+
+from pydantic import BaseModel
+
+
+class LogConfig(BaseModel):
+    """Logging configuration to be set for the server"""
+
+    LOGGER_NAME: str = "mycoolapp"
+    LOG_FORMAT: str = "%(levelprefix)s %(message)s"
+    LOG_LEVEL: str = "DEBUG"
+
+    # Logging config
+    version: int = 1
+    disable_existing_loggers: bool = False
+    formatters: dict = {
+        "default": {
+            "()": "uvicorn.logging.DefaultFormatter",
+            "fmt": LOG_FORMAT,
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    }
+    handlers: dict = {
+        "default": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stderr",
+        },
+    }
+    loggers: dict = {
+        LOGGER_NAME: {"handlers": ["default"], "level": LOG_LEVEL},
+    }
+
+
+def getLogger():
+    dictConfig(LogConfig().model_dump())
+    logger = logging.getLogger("mycoolapp")
+
+    return logger

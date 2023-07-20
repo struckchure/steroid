bb85ab299aab7338c6388f020be3fc9e02ea19b3
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-07-20 15:44:35 +0100

    docs(example.py): updates example file

--- example.py
+++ example.py
@@ -1,8 +1,10 @@
+from typing import Union
+
 import pydantic
+from fastapi import Header, HTTPException, Request
 from typing_extensions import TypedDict
 
-from steroid.app import CreateApp
-from steroid.common import Controller, Get, Post
+from steroid import Controller, CreateApp, Get, Middleware, Post, UseMiddlewares
 
 
 class Salute(pydantic.BaseModel):
@@ -12,6 +14,7 @@ class Salute(pydantic.BaseModel):
 class Hello(TypedDict):
     pk: str
     gID: int
+    XToken: Union[str, None]
 
 
 class GreetResponse(TypedDict):
@@ -26,18 +29,57 @@ class NoGreetResponse(TypedDict):
     noHello: NoHello
 
 
-@Controller()
+class AuthMiddleware(Middleware):
+    HOOK = "PRE"
+
+    def action(request: Request):
+        authToken = request.headers.get("Authorization")
+        if not authToken:
+            raise HTTPException(status_code=401, detail="Unauthorized")
+
+
+class PreMiddleware(Middleware):
+    HOOK = "PRE"
+
+    def action(request: Request):
+        print("hello")
+
+
+class PostMiddleware(Middleware):
+    HOOK = "POST"
+
+    def action(request: Request):
+        print("bye")
+
+
+@UseMiddlewares(AuthMiddleware)
+@Controller("greetings")
 class GreetController:
     @Get("greet/{pk}/{gID}")
-    def Greet(pk: str, gID: int) -> GreetResponse:
-        return {"hello": {"pk": pk, "gID": gID}}
+    def Greet(
+        pk: str,
+        gID: int,
+        x_token: str = Header(default=None),
+    ) -> GreetResponse:
+        return {"hello": {"pk": pk, "gID": gID, "XToken": x_token}}
 
     @Post("no-greet")
     def NoGreet(salute: Salute) -> NoGreetResponse:
         return {"noHello": salute}
 
 
+@UseMiddlewares(PreMiddleware, PostMiddleware)
+@Controller("clutch")
+class CluthController:
+    @Get("savage")
+    def Savage():
+        return {"hello": "clutchers, savage, and everything else"}
+
+
 app = CreateApp()
 
 app.addController(GreetController)
-app.start()
+app.addController(CluthController)
+
+if __name__ == "__main__":
+    app.start()

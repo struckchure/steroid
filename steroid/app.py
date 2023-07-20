21b36a4f2acd641e4b29b156ce48d23739622dc8
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-07-20 15:42:23 +0100

    feat(steroid/app): adds hot reload with `watchfiles`

--- steroid/app.py
+++ steroid/app.py
@@ -1,14 +1,24 @@
+import os
+
 import uvicorn
 from fastapi import FastAPI
+from watchfiles import Change, DefaultFilter, run_process
 
-from steroid.common import Controller
-from steroid.utils import removeLeadingOrTrailingSlash
+from steroid.controller import Controller
+from steroid.logging import getLogger
+from steroid.utils import clearTerminal, removeLeadingOrTrailingSlash
 
-app = FastAPI()
+
+class OnlyPythonFilesFilter(DefaultFilter):
+    allowed_extensions = ".py"
+
+    def __call__(self, change: Change, path: str) -> bool:
+        return super().__call__(change, path) and path.endswith(self.allowed_extensions)
 
 
 class CreateApp:
     _APP: FastAPI = None
+    logger = getLogger()
 
     def __new__(cls, *args, **kwargs):
         if cls._APP is None:
@@ -22,8 +32,29 @@ class CreateApp:
         return cls._APP
 
     @classmethod
-    def start(cls, *args, **kwargs):
-        uvicorn.run(cls.app, *args, **kwargs)
+    def _start(cls):
+        clearTerminal()
+
+        uvicorn.run(cls.app)
+
+    @classmethod
+    def onFileChange(cls, change: Change):
+        getLogger().info(f"reloading ... file changes in {change}")
+
+    @classmethod
+    def start(cls, reload=True):
+        if reload == False:
+            cls._start()
+            return
+
+        watchFilePath = os.getcwd()
+
+        run_process(
+            watchFilePath,
+            target=cls._start,
+            callback=cls.onFileChange,
+            watch_filter=OnlyPythonFilesFilter(),
+        )
 
     @classmethod
     def addController(cls, controller: Controller):
@@ -34,3 +65,4 @@ class CreateApp:
             if controller.path
             else [],
         )
+        cls.logger.info(f"Mapped {controller.path} to router")

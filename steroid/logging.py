9a24b48c06b015ef4726ae42975e336928665f40
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-07-23 18:49:10 +0100

    feat(logging): adds logging configurations

--- steroid/logging.py
+++ steroid/logging.py
@@ -7,18 +7,27 @@ from pydantic import BaseModel
 class LogConfig(BaseModel):
     """Logging configuration to be set for the server"""
 
-    LOGGER_NAME: str = "mycoolapp"
-    LOG_FORMAT: str = "%(levelprefix)s %(message)s"
-    LOG_LEVEL: str = "DEBUG"
+    DATE_FORMAT: str = "%d/%m/%Y, %H:%M:%S"
 
-    # Logging config
+    LOG_NAME: str = "steroid"
+    LOG_FORMAT: str = "[%(asctime)s] %(levelname)s %(message)s"
+    LOG_LEVEL: str = "INFO"
+
+    UVICORN_ACCESS_LOG_FORMAT: str = '[%(asctime)s] %(levelname)s [Uvicorn] %(client_addr)s - "%(request_line)s" %(status_code)s'
+
     version: int = 1
     disable_existing_loggers: bool = False
     formatters: dict = {
         "default": {
             "()": "uvicorn.logging.DefaultFormatter",
             "fmt": LOG_FORMAT,
-            "datefmt": "%Y-%m-%d %H:%M:%S",
+            "datefmt": DATE_FORMAT,
+        },
+        "access": {
+            "()": "uvicorn.logging.AccessFormatter",
+            "fmt": UVICORN_ACCESS_LOG_FORMAT,
+            "datefmt": DATE_FORMAT,
+            "use_colors": False,
         },
     }
     handlers: dict = {
@@ -27,14 +36,56 @@ class LogConfig(BaseModel):
             "class": "logging.StreamHandler",
             "stream": "ext://sys.stderr",
         },
+        "access": {
+            "formatter": "access",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stderr",
+        },
+        "error": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stderr",
+        },
     }
     loggers: dict = {
-        LOGGER_NAME: {"handlers": ["default"], "level": LOG_LEVEL},
+        LOG_NAME: {
+            "handlers": ["default"],
+            "level": LOG_LEVEL,
+        },
+        "uvicorn.access": {
+            "handlers": ["access"],
+            "level": LOG_LEVEL,
+        },
+        "uvicorn.error": {
+            "handlers": ["error"],
+            "level": LOG_LEVEL,
+        },
     }
 
 
-def getLogger():
-    dictConfig(LogConfig().model_dump())
-    logger = logging.getLogger("mycoolapp")
+def getLogConfig():
+    return LogConfig().model_dump()
+
+
+logger = None
+
+
+def getLogger(disableAccessLog: bool = False, disableErrorLog: bool = True):
+    """
+    If the logger is not created yet, create it.
+    Otherwise, return the logger.
+    This is to avoid creating multiple loggers.
+    This is a singleton pattern.
+    https://en.wikipedia.org/wiki/Singleton_pattern
+    """
+
+    global logger
+
+    if not logger:
+        dictConfig(getLogConfig())
+        logger = logging.getLogger(LogConfig().LOG_NAME)
+
+        logging.getLogger("uvicorn.access").disabled = disableAccessLog
+        logging.getLogger("uvicorn.error").disabled = disableErrorLog
 
     return logger

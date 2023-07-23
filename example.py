d6ef71aedd8b70b105fbbbfedc1b385b7809785c
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-07-23 19:14:10 +0100

    docs(example.py): adds middleware samples

--- example.py
+++ example.py
@@ -1,8 +1,5 @@
-from typing import Union
-
 import pydantic
 from fastapi import Header, HTTPException, Request
-from typing_extensions import TypedDict
 
 from steroid import Controller, CreateApp, Get, Middleware, Post, UseMiddlewares
 
@@ -11,30 +8,12 @@ class Salute(pydantic.BaseModel):
     aye: str
 
 
-class Hello(TypedDict):
-    pk: str
-    gID: int
-    XToken: Union[str, None]
-
-
-class GreetResponse(TypedDict):
-    hello: Hello
-
-
-class NoHello(TypedDict):
-    aye: str
-
-
-class NoGreetResponse(TypedDict):
-    noHello: NoHello
-
-
 class AuthMiddleware(Middleware):
-    HOOK = "PRE"
+    HOOK = "POST"
 
     def action(request: Request):
         authToken = request.headers.get("Authorization")
-        if not authToken:
+        if authToken is None or not authToken:
             raise HTTPException(status_code=401, detail="Unauthorized")
 
 
@@ -52,34 +31,41 @@ class PostMiddleware(Middleware):
         print("bye")
 
 
-@UseMiddlewares(AuthMiddleware)
 @Controller("greetings")
 class GreetController:
     @Get("greet/{pk}/{gID}")
     def Greet(
         pk: str,
         gID: int,
-        x_token: str = Header(default=None),
-    ) -> GreetResponse:
-        return {"hello": {"pk": pk, "gID": gID, "XToken": x_token}}
+        authorization: str = Header(alias="Authorization", default=None),
+    ):
+        return {"hello": {"pk": pk, "gID": gID, "XToken": authorization}}
 
+    @UseMiddlewares(AuthMiddleware)
     @Post("no-greet")
-    def NoGreet(salute: Salute) -> NoGreetResponse:
+    def NoGreet(salute: Salute):
         return {"noHello": salute}
 
 
-@UseMiddlewares(PreMiddleware, PostMiddleware)
 @Controller("clutch")
 class CluthController:
+    @UseMiddlewares(PreMiddleware, PostMiddleware)
     @Get("savage")
     def Savage():
         return {"hello": "clutchers, savage, and everything else"}
 
+    @Get("not-a-savage")
+    def NotASavage():
+        return {"hello": "clutchers, not a savage, and everything else"}
 
-app = CreateApp()
 
-app.addController(GreetController)
-app.addController(CluthController)
+def main():
+    app = CreateApp()
+
+    app.addController(GreetController)
+    app.addController(CluthController)
+    app.start()
+
 
 if __name__ == "__main__":
-    app.start()
+    main()
